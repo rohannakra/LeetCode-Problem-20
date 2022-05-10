790f920dc5e56cdd9bcc76b2f9932a4ad4ba897e
RoJuan230 <rohan.nakra100@gmail.com>
2022-05-10 09:23:47 -0700

    Adding files

---
+++ main.py
@@ -0,0 +1,55 @@
+def is_valid(string):
+    lst = [char for char in string]
+    opposite = ""
+    try:
+        for i in range(int(len(string)/2)):
+            if lst[0] == '(':
+                opposite = ")"
+            elif lst[0] == "[":
+                opposite = "]"
+            elif lst[0] == "{":
+                opposite = "}"
+            else:
+                return False
+            lst.remove(lst[0])
+            try:
+                lst.remove(opposite)
+            except ValueError:    # Couldn't find match
+                return False
+    except TypeError:    # This means that there is an uneven amount of open and closed characters... invalid.
+        return False
+    if len(lst) != 0:
+        return False
+    return True
+
+print(is_valid("()"))
+print(is_valid("(]"))
+print(is_valid('((((()))))'))
+print(is_valid("()[]{}"))
+
+# ---- Accepted LeetCode Solution ----
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        lst = [char for char in s]
+        opposite = ""
+        try:
+            for i in range(int(len(s)/2)):
+                if lst[0] == '(':
+                    opposite = ")"
+                elif lst[0] == "[":
+                    opposite = "]"
+                elif lst[0] == "{":
+                    opposite = "}"
+                else:
+                    return False
+                lst.remove(lst[0])
+                try:
+                    lst.remove(opposite)
+                except ValueError:    # Couldn't find match
+                    return False
+        except TypeError:    # This means that there is an uneven amount of open and closed characters... invalid.
+            return False
+        if len(lst) != 0:
+            return False
+        return True
